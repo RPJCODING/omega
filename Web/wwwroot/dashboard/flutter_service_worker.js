407104b62edaa88f9f25288c003214574b7688f5
Rodrigo <rpjcoding@gmail.com>
2022-03-26 04:19:09 -0300

    WIP

--- Web/wwwroot/dashboard/flutter_service_worker.js
+++ Web/wwwroot/dashboard/flutter_service_worker.js
@@ -42,9 +42,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e69d5d31b68a255eeab748727409e298",
-"/": "e69d5d31b68a255eeab748727409e298",
-"main.dart.js": "89ff17bbb92a819c1f3e5d489ca4c0aa",
+"index.html": "7db87baf413553f4596f953315f50def",
+"/": "7db87baf413553f4596f953315f50def",
+"main.dart.js": "3c2bd904aa2629d7c474567e1052f7f3",
 "manifest.json": "25c2a5f8c70e96d25d503a427e8d7f65",
 "version.json": "bb85e32a1929029013d07ba0b1ad35e3"
 };
